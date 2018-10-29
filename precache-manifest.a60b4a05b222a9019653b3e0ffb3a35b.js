38b858092cbed7a70d5e1640038f8a5206af3627
Mathavan <math08avan@gmail.com>
2018-10-29 22:01:54 +0800

    Updates

---
+++ precache-manifest.a60b4a05b222a9019653b3e0ffb3a35b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "635860fd533afff61498",
+    "url": "/contact_manager/static/js/runtime~main.635860fd.js"
+  },
+  {
+    "revision": "c45e650da2ad068aba83",
+    "url": "/contact_manager/static/js/main.c45e650d.chunk.js"
+  },
+  {
+    "revision": "3ebc2a4edcb29d99d682",
+    "url": "/contact_manager/static/js/1.3ebc2a4e.chunk.js"
+  },
+  {
+    "revision": "c45e650da2ad068aba83",
+    "url": "/contact_manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "3ebc2a4edcb29d99d682",
+    "url": "/contact_manager/static/css/1.6f0c370b.chunk.css"
+  },
+  {
+    "revision": "b0fcd18e59907480b3aa0d40c43b67dd",
+    "url": "/contact_manager/index.html"
+  }
+];
